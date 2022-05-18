59597e3db55e78cdb6f1dc70d5821902acb53cbb
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-05-18 16:44:14 +0300

    1

---
+++ src/test/java/pageobjects/SearchPage.java
@@ -0,0 +1,42 @@
+package pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import java.time.Duration;
+
+public class SearchPage extends AbstractPage {
+    @FindBy(css = "[class='btn btn-bord more-par-btn']")
+    private WebElement buttonParameters;
+    @FindBy(css = "[data-title]")
+    private WebElement dropDownSelectDistrict;
+    @FindBy(xpath = "//*[text()='Центральный район']")
+    private WebElement district;
+    @FindBy(css="[class='btn btn-primary btn-multi select-modal-filter']")
+    private WebElement buttonSelect;
+    @FindBy(css="[class='ext-filter_close-btn']")
+    private  WebElement buttonCloseParameters;
+
+
+    public SearchPage clickButtonParameters() {
+        buttonParameters.click();
+        return this;
+    }
+
+    public SearchPage chooseDistrict() {
+        dropDownSelectDistrict.click();
+        Actions action = new Actions(driver);
+        action.pause(Duration.ofSeconds(1)).moveToElement(district).click().build().perform();
+        return this;
+    }
+    public SearchPage clickButtonSelect(){
+        buttonSelect.click();
+        return this;
+    }
+    public SearchPage clickButtonCloseParameters(){
+        waitForElementToBeClickable(buttonCloseParameters).click();
+        return this;
+    }
+}
+
---
+++ src/test/java/test/DomovitaTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+import pageobjects.HomePage;
+
+public class DomovitaTest extends AbstractTest {
+    @Test
+    public void testMinFlatRent() {
+
+        new HomePage().openPage().chooseOperationRent().chooseNumberOfRooms("1").clickButtonSearch()
+                .clickButtonParameters().chooseDistrict();
+    }
+}
